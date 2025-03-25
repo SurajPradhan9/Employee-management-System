0168eaf219f78cd673140a0c8b5456518fc172ea
Suraj Pradhan <surajpradhan9104@gmail.com>
2025-03-25 14:56:54 +0530

    Employee Dashboard Done

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div
+      id="tasklist"
+      className=" flex overflow-x-auto items-center justify-start h-[55%] gap-5 flex-nowrap py-5 mt-10 w-full "
+    >
+
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-red-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">Jan 9 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold"> Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima quos
+          sint excepturi tempore mollitia vitae.
+        </p>
+      </div>
+
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-blue-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">Jan 9 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold"> Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima quos
+          sint excepturi tempore mollitia vitae.
+        </p>
+      </div>
+
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-green-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">Jan 9 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold"> Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima quos
+          sint excepturi tempore mollitia vitae.
+        </p>
+      </div>
+
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-yellow-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">Jan 9 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold"> Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima quos
+          sint excepturi tempore mollitia vitae.
+        </p>
+      </div>
+
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-pink-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">Jan 9 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold"> Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima quos
+          sint excepturi tempore mollitia vitae.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
